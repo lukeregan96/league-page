b761c5475a1799b48ec4bdf4f571a06f82284fbd
lukeregan96 <141790074+lukeregan96@users.noreply.github.com>
2023-08-08 21:13:22 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -29,7 +29,7 @@ export const managers = [
       "name": "Luke Regan",
       "location": "Pittsburgh", // (optional)
       "bio": "Before three notes of the pipe came fluttering you heard a sound like armies muttering, and the muttering grew to a grumbling, and the grumbling grew to a mighty rumbling, and out of the houses the rats came tumbling.",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/luke.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
